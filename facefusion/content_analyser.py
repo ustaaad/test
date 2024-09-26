0f7a22617317b597eb3794da2ab87c32687761b6
ustaaad <155310901+ustaaad@users.noreply.github.com>
2024-09-26 09:15:34 -0400

    Update content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -74,19 +74,12 @@ def analyse_frame(vision_frame : VisionFrame) -> bool:
 	vision_frame = prepare_frame(vision_frame)
 	probability = forward(vision_frame)
 
-	return probability > PROBABILITY_LIMIT
+	return False
 
 
 def forward(vision_frame : VisionFrame) -> float:
-	content_analyser = get_inference_pool().get('content_analyser')
 
-	with conditional_thread_semaphore():
-		probability = content_analyser.run(None,
-		{
-			'input': vision_frame
-		})[0][0][1]
-
-	return probability
+	return False
 
 
 def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
